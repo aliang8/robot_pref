504f426958735fa8f4f161add0cd4043b96f65ec
Anthony Liang <aliangdw@umich.edu>
2025-05-20 20:49:57 +0000

    fix iql mw

--- COMMANDS.sh
+++ COMMANDS.sh
@@ -1,13 +1,9 @@
+# OLD
 python preprocess_segments.py --max_segments 2000 --use_relative_differences --output_file preprocessed/assembly_v2_segments.pt
 
 python eef_clustering.py --preprocessed_data preprocessed/assembly_v2_segments.pt --n_clusters 3 --max_dtw_segments 500 --output_dir results/eef_clustering --use_relative_differences --skip_videos --linkage_method ward
 
 python collect_cluster_preferences.py --preprocessed_data preprocessed/assembly_v2_segments.pt --clustering_results results/eef_clustering/clustering_results.pkl --output_dir results/preferences
-
-# make mixed expertise dataset
-python create_mixed_expertise_dataset.py \
-    --data_path=/scr/shared/clam/datasets/metaworld/assembly-v2/buffer_assembly-v2_clean.pt \
-    --output_dir=dataset_mw
 
 python preprocess_dtw_matrix.py \
     --data_path=/scr/shared/clam/datasets/metaworld/assembly-v2/buffer_assembly-v2_clean.pt \
@@ -20,7 +16,7 @@ python train_reward_model.py \
 
 python train_reward_model_active.py \
     data.data_path=/scr/shared/clam/datasets/metaworld/assembly-v2/buffer_assembly-v2_clean.pt \
-    active_learning.uncertainty_method=disagreement \
+    active_learning.uncertainty_method=entropy \
     dtw_augmentation.enabled=true \
     --multirun
 
@@ -37,19 +33,23 @@ python train_policy.py \
 python train_policy.py \
     --config-name=iql \
     data.data_path=/scr/shared/clam/datasets/metaworld/assembly-v2/buffer_assembly-v2_clean.pt \
-    data.reward_model_path=/scr/shared/clam/robot_pref/results/reward_model/state_action_reward_model.pt \
     wandb.use_wandb=true \
+    data.use_zero_rewards=true \
     model.weight_temp=1.0 \
     random_seed=521,522,523 \
+    hydra/launcher=slurm \
     --multirun
 
+    data.reward_model_path=/scr/shared/clam/robot_pref/results/reward_model/state_action_reward_model.pt \
 
 
 
 
 # using balanced dataset
-
-
+# make mixed expertise dataset
+python create_mixed_expertise_dataset.py \
+    --data_path=/scr/shared/clam/datasets/metaworld/assembly-v2/buffer_assembly-v2_clean.pt \
+    --output_dir=dataset_mw
 
 # train reward model without active learning
 python train_reward_model.py \
@@ -87,6 +87,16 @@ python train_policy.py \
     hydra/launcher=slurm \
     --multirun
 
+# baseline - zero rewards
+python train_policy.py \
+    --config-name=iql \
+    data.data_path=/scr/aliang80/robot_pref/dataset_mw/buffer_assembly-v2_balanced.pt \
+    wandb.use_wandb=true \
+    data.use_zero_rewards=true \
+    random_seed=521,522,523 \
+    hydra/launcher=slurm \
+    --multirun
+
 # baseline - BC
 # BC
 python train_policy.py \
@@ -110,7 +120,7 @@ python train_policy.py \
 python train_reward_model_active.py \
     data.data_path=/scr/aliang80/robot_pref/dataset_mw/buffer_assembly-v2_balanced.pt \
     active_learning.uncertainty_method=entropy,disagreement \
-    active_learning.total_queries=300 \
+    active_learning.max_queries=50 \
     dtw_augmentation.enabled=true \
     hydra/launcher=slurm \
     --multirun
